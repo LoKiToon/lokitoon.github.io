2f678d073b120868f95947b99020362b9e15d0d3
nk <98922449+LoKiToon@users.noreply.github.com>
2024-01-01 18:23:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# personal-website
+This is a website that was created mainly to showcase artworks out of my passion.
+
+# TODO
+* A refactoring of the Expression playground ( in progress )
+* Maybe implement lazy loading? Not sure..
+* Dark/light themes
---
+++ menuloader.js
@@ -0,0 +1,24 @@
+const menuElement = document.getElementById("menu")
+const menuButtons = [
+  // HREF, Icon, Title
+  ["index.html", "resources/Home.svg", "Home"],
+  ["portfolio.html", "resources/Folder.svg", "Portfolio"],
+  ["expressionplayground.html", "resources/Faces.svg", "Expression Playground"],
+  ["othersites.html", "resources/User.svg", "My Friends' Websites"],
+  ["fanart.html", "resources/Palette.svg", "Fan-Art"],
+  ["downloads.html", "resources/Download.svg", "Public Downloads"]
+]
+
+let i
+
+for (i = 0; i <= menuButtons.length - 1; i++) {
+  let link = document.createElement("a")
+  link.className = "menubutton"
+  link.href = menuButtons[i][0]
+  link.title = menuButtons[i][2]
+  menuElement.appendChild(link)
+  
+  let image = document.createElement("img")
+  image.src = menuButtons[i][1]
+  link.appendChild(image)
+}
